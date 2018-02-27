380ff2082d94a4200424673833662c4d682b38ba
Rani Shah <rani76@gmail.com>
2018-02-27 23:22:29 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,47 @@
-hopper = {
-  :known_for => "COBOL",
-  :languages => ["COBOL", "FORTRAN"]
-}
+def hopper 
+programmer_hash = 
+{
+  hopper: {
+    known_for: "COBOL",
+    languages: ["COBOL", "FORTRAN"]
+  }
+ alan_kay: {
+   known_for: "Object Orientation", 
+   languages: ["Smalltalk", "LISP"]
+ }
+  dennis_ritchie: {
+    known_for: "Unix"
+    languages: ["C"]
+  }
+  }
+
+  programmer_hash[dennis_ritchie]
+
+end 
+
+def dennis_ritchies_language
+  programmer_hash = 
+   {
+       :grace_hopper => {
+         :known_for => "COBOL",
+         :languages => ["COBOL", "FORTRAN"]
+       },
+       :alan_kay => {
+         :known_for => "Object Orientation",
+         :languages => ["Smalltalk", "LISP"]
+       },
+       :dennis_ritchie => {
+         :known_for => "Unix",
+         :languages => ["C"]
+       }
+    }
+
+    programmer_hash[:dennis_ritchie][:languages]
+
+  end 
+
+  def adding_matz
+   programmer_hash[:yukihiro_matsumoto] = {}
+  end 
+  
+  
